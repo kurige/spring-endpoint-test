9694ca9258899bc77543694595a1673f9d5fac55
Chris Gateley <me@cgateley.com>
2019-06-11 14:40:35 -0400

    Spit out entire document

--- src/main/java/com/cg/endpointtest/FreqController.java
+++ src/main/java/com/cg/endpointtest/FreqController.java
@@ -1,15 +1,31 @@
 package com.cg.endpointtest;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.nio.file.Files;
+
 @Controller
 public class FreqController {
+
+    @Value("classpath:data/freq.txt")
+    Resource freqDocument;
 
     @GetMapping("/freq")
     @ResponseBody
     public String frequency() {
-        return "Hello World";
+        return readFrequencyData();
+    }
+
+    private String readFrequencyData() {
+        try {
+            return new String(Files.readAllBytes(freqDocument.getFile().toPath()));
+        } catch (IOException e) {
+            return String.format("Error reading frequency document: %s", e.getMessage());
+        }
     }
 }
