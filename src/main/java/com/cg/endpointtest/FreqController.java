93fedc73f10ef5597ad296e485cf8f6c38eddcc1
Chris Gateley <me@cgateley.com>
2019-06-12 13:46:57 -0400

    Load frequency data only once on first request

--- src/main/java/com/cg/endpointtest/FreqController.java
+++ src/main/java/com/cg/endpointtest/FreqController.java
@@ -3,14 +3,12 @@ package com.cg.endpointtest;
 import com.cg.endpointtest.model.FreqList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.IOException;
 import java.util.Map;
 
 @Controller
@@ -18,20 +16,14 @@ public class FreqController {
 
     private final Logger logger = LoggerFactory.getLogger(FreqController.class);
 
-    @Value("classpath:data/freq.txt")
-    Resource freqDocument;
+    @Autowired
+    FreqList freqList;
 
     @GetMapping("/freq")
     @ResponseBody
     public ModelAndView displayFrequencyData(Map<String, Object> model) {
 
-        FreqList items = new FreqList();
-        try {
-            items = FreqList.parseFile(freqDocument.getFile());
-        } catch (IOException e) {
-            logger.error("Error while loading frequency data: {}", e.getMessage());
-        }
-        model.put("freqItems", items);
+        model.put("freqItems", freqList.getItems());
         return new ModelAndView("freq.tmpl", model);
     }
 }
--- src/main/java/com/cg/endpointtest/model/FreqList.java
+++ src/main/java/com/cg/endpointtest/model/FreqList.java
@@ -2,18 +2,38 @@ package com.cg.endpointtest.model;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
-public class FreqList extends LinkedList<FreqItem> {
+@Component
+public class FreqList {
 
-    private static final Logger logger = LoggerFactory.getLogger(FreqList.class);
+    private final Logger logger = LoggerFactory.getLogger(FreqList.class);
 
-    public static FreqList parseFile(File csvFile) {
-        FreqList retval = new FreqList();
+    private List<FreqItem> items;
+
+    @Autowired
+    public FreqList(@Value("classpath:data/freq.txt") Resource freqDocument) {
+        items = new LinkedList<>();
+
+        try {
+            parseFile(freqDocument.getFile());
+        } catch (IOException e) {
+            logger.error("Error while loading frequency data: {}", e.getMessage());
+        }
+    }
+
+    public void parseFile(File csvFile) {
+        logger.debug("Parsing CSV file: {}", csvFile.getName());
         try (Scanner scanner = new Scanner(csvFile)) {
             while (scanner.hasNextLine()) {
                 try (Scanner rowScanner = new Scanner(scanner.nextLine())) {
@@ -22,13 +42,15 @@ public class FreqList extends LinkedList<FreqItem> {
                     int count = rowScanner.nextInt();
                     double freq = rowScanner.nextDouble();
 
-                    retval.add(new FreqItem(word, count, freq));
+                    items.add(new FreqItem(word, count, freq));
                 }
             }
         } catch (FileNotFoundException e) {
             logger.error("Failed to find CSV file: {}", csvFile.toPath());
         }
+    }
 
-        return retval;
+    public List<FreqItem> getItems() {
+        return items;
     }
 }
